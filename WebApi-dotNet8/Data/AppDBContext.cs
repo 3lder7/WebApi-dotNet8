17bae984262e1c2b1e2d712573549206bc8b5d4c
Elder <eldergalvao07@gmail.com>
2024-11-05 20:31:46 -0300

    Adicionar arquivos de projeto.

---
+++ WebApi-dotNet8/Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi_dotNet8.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)//opções de conexão
+        {
+            
+        }
+    }
+}
